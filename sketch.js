f400f501d259ff54ca2cb1f919527323709f5376
samblackmore <samblackmore07@gmail.com>
2016-10-19 23:11:20 -0400

    optimizing with global vars

--- sketch.js
+++ sketch.js
@@ -1,15 +1,14 @@
+var gridSize = 16,
+    cellSize;
+
 function setup() {
   createCanvas(640, 480);
+  background(200);
+  var minSide = (width < height ? width : height) - 1;
+  cellSize = Math.floor(minSide / gridSize);
 }
 
 function draw() {
-
-  var gridSize = 16,
-      minSide = (width < height ? width : height) - 1,
-      cellSize = Math.floor(minSide / gridSize);
-
-  background(200);
-
   for (var i = 0; i < gridSize; i++)
     for (var j = 0; j < gridSize; j++)
       rect(i * cellSize, j * cellSize, cellSize, cellSize);
