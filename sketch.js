71b97ce4b37404abcfcf88cce45b6c09ad89b540
samblackmore <samblackmore07@gmail.com>
2016-10-19 23:01:08 -0400

    drawing grid

--- sketch.js
+++ sketch.js
@@ -1,7 +1,16 @@
 function setup() {
-
+  createCanvas(640, 480);
 }
 
 function draw() {
-  ellipse(50, 50, 80, 80);
+
+  var gridSize = 16,
+      minSide = (width < height ? width : height) - 1,
+      cellSize = Math.floor(minSide / gridSize);
+
+  background(200);
+
+  for (var i = 0; i < gridSize; i++)
+    for (var j = 0; j < gridSize; j++)
+      rect(i * cellSize, j * cellSize, cellSize, cellSize);
 }
