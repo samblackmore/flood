f9bd2b48b2ca15f6776170c1d299c98065aacc14
samblackmore <samblackmore07@gmail.com>
2016-11-08 21:59:55 -0500

    adding grid and mouse objects :mouse:

--- sketch.js
+++ sketch.js
@@ -1,32 +1,55 @@
 var gridSize = 16,
-    cellSize,
-    bkCol = 200;
+    cellSize = 48,
+    bkCol = 200,
+    mouse;
 
 var maze = [["NW", "NE", "NW", "NS", "NS", "NS", "NS", "NS", "N", "N", "NS", "NS", "NS", "NS", "N", "NE"], ["WE", "WE", "WS", "NE", "NW", "NS", "NS", "NES", "WE", "W", "NS", "NS", "NS", "NE", "WE", "WE"], ["WE", "WS", "N", "ES", "WS", "NS", "NS", "NS", "ES", "WE", "NWS", "NS", "NS", "E", "WE", "WE"], ["WE", "NW", "E", "NW", "NS", "N", "NS", "NS", "NS", "S", "NS", "NS", "NS", "ES", "WE", "WE"], ["WE", "WE", "WS", "E", "NWE", "W", "NS", "NS", "NS", "NS", "NS", "N", "NE", "NW", "E", "WE"], ["WE", "WS", "NE", "W", "E", "W", "NS", "N", "NS", "NS", "NE", "WE", "WE", "WES", "WE", "WE"], ["W", "N", "ES", "WE", "WE", "WE", "NW", "ES", "NW", "NE", "WS", "E", "W", "NS", "ES", "WE"], ["WE", "WS", "NE", "WE", "WE", "WE", "WE", "NW", "E", "WS", "NE", "WE", "WS", "NE", "NW", "E"], ["WE", "NW", "E", "WE", "WE", "WE", "WE", "WS", "ES", "NW", "ES", "WS", "NE", "WE", "WE", "WE"], ["WE", "WE", "WE", "WS", "ES", "WE", "WS", "NS", "NE", "WS", "NE", "NW", "ES", "WE", "WE", "WE"], ["WE", "WE", "WE", "NWS", "NS", "S", "NS", "NE", "WS", "NE", "W", "ES", "NW", "E", "WE", "WE"], ["WE", "WE", "W", "NS", "NE", "NW", "NE", "WS", "NE", "WS", "E", "NW", "ES", "WE", "WE", "WE"], ["WS", "E", "WS", "NES", "WE", "WE", "WS", "NE", "WS", "N", "S", "S", "NS", "ES", "WE", "WE"], ["NW", "E", "NWS", "NS", "E", "WS", "NE", "WS", "NE", "WS", "NS", "NS", "NS", "NS", "E", "WE"], ["WE", "W", "NS", "NS", "ES", "NWE", "WS", "NE", "WS", "NS", "NS", "NS", "NS", "NS", "ES", "WE"], ["WES", "WS", "NS", "NS", "NS", "S", "NS", "S", "NS", "NS", "NS", "NS", "NS", "NS", "NS", "ES"]]
 
 function setup() {
-  createCanvas(640, 480);
+  createCanvas(gridSize*cellSize+1, gridSize*cellSize+1);
   background(bkCol);
-  var minSide = (width < height ? width : height) - 1;
-  cellSize = Math.floor(minSide / gridSize);
+  //var minSide = (width < height ? width : height) - 1;
+  //cellSize = Math.floor(minSide / gridSize);
+  grid = new Grid(maze);
+  mouse = new Character(0, gridSize-1);
 }
 
 function draw() {
-  fill(bkCol + 10);
-  stroke(bkCol - 10);
-  for (var i = 0; i < gridSize; i++) {
-    for (var j = 0; j < gridSize; j++) {
-      var d = cellSize,
-          x = j * d,
-          y = i * d,
-          dir = maze[i][j];
-      stroke(bkCol - 10);
-      rect(x, y, d, d);
-      stroke(0);
-      if (dir.indexOf('N') > -1) line(x, y, x + d, y);
-      if (dir.indexOf('E') > -1) line(x + d, y, x + d, y + d);
-      if (dir.indexOf('S') > -1) line(x, y + d, x + d, y + d);
-      if (dir.indexOf('W') > -1) line(x, y, x, y + d);
-    }
+  grid.draw();
+  mouse.draw();
+}
+
+function found(place, thing) {
+  return place.indexOf(thing) != -1;
+}
+
+function Grid(walls) {
+  this.walls = walls;
+
+  this.draw = function() {
+    this.walls.forEach(function(row, i) {
+      row.forEach(function(cell, j) {
+        var d = cellSize, x = j * d, y = i * d;
+
+        fill(bkCol + 10).stroke(bkCol - 10).rect(x, y, d, d);   //outline
+        stroke(0);
+        if (found(cell, 'N')) line(x, y, x + d, y);
+        if (found(cell, 'E')) line(x + d, y, x + d, y + d);
+        if (found(cell, 'S')) line(x, y + d, x + d, y + d);
+        if (found(cell, 'W')) line(x, y, x, y + d);
+      });
+    });
+  }
+}
+
+function Character(row, col) {
+  var offset = cellSize/2;
+  this.row = row;
+  this.col = col;
+  this.x = this.row * cellSize + offset;
+  this.y = this.col * cellSize + offset;
+  this.diameter = cellSize/3;
+  this.draw = function() {
+    fill(255, 204, 0).stroke(0).strokeWeight(1).ellipse(this.x, this.y, this.diameter, this.diameter);
   }
 }
