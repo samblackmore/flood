cf83b1cd09253c95c6d3473154d4369322f90440
samblackmore <samblackmore07@gmail.com>
2016-11-09 00:18:34 -0500

    implementing recursive flood fill

--- sketch.js
+++ sketch.js
@@ -1,7 +1,8 @@
 var gridSize = 16,
     cellSize = 48,
     bkCol = 200,
-    mouse;
+    mouse,
+    maxDist = 0;
 
 var maze = [["NW", "NE", "NW", "NS", "NS", "NS", "NS", "NS", "N", "N", "NS", "NS", "NS", "NS", "N", "NE"], ["WE", "WE", "WS", "NE", "NW", "NS", "NS", "NES", "WE", "W", "NS", "NS", "NS", "NE", "WE", "WE"], ["WE", "WS", "N", "ES", "WS", "NS", "NS", "NS", "ES", "WE", "NWS", "NS", "NS", "E", "WE", "WE"], ["WE", "NW", "E", "NW", "NS", "N", "NS", "NS", "NS", "S", "NS", "NS", "NS", "ES", "WE", "WE"], ["WE", "WE", "WS", "E", "NWE", "W", "NS", "NS", "NS", "NS", "NS", "N", "NE", "NW", "E", "WE"], ["WE", "WS", "NE", "W", "E", "W", "NS", "N", "NS", "NS", "NE", "WE", "WE", "WES", "WE", "WE"], ["W", "N", "ES", "WE", "WE", "WE", "NW", "ES", "NW", "NE", "WS", "E", "W", "NS", "ES", "WE"], ["WE", "WS", "NE", "WE", "WE", "WE", "WE", "NW", "E", "WS", "NE", "WE", "WS", "NE", "NW", "E"], ["WE", "NW", "E", "WE", "WE", "WE", "WE", "WS", "ES", "NW", "ES", "WS", "NE", "WE", "WE", "WE"], ["WE", "WE", "WE", "WS", "ES", "WE", "WS", "NS", "NE", "WS", "NE", "NW", "ES", "WE", "WE", "WE"], ["WE", "WE", "WE", "NWS", "NS", "S", "NS", "NE", "WS", "NE", "W", "ES", "NW", "E", "WE", "WE"], ["WE", "WE", "W", "NS", "NE", "NW", "NE", "WS", "NE", "WS", "E", "NW", "ES", "WE", "WE", "WE"], ["WS", "E", "WS", "NES", "WE", "WE", "WS", "NE", "WS", "N", "S", "S", "NS", "ES", "WE", "WE"], ["NW", "E", "NWS", "NS", "E", "WS", "NE", "WS", "NE", "WS", "NS", "NS", "NS", "NS", "E", "WE"], ["WE", "W", "NS", "NS", "ES", "NWE", "WS", "NE", "WS", "NS", "NS", "NS", "NS", "NS", "ES", "WE"], ["WES", "WS", "NS", "NS", "NS", "S", "NS", "S", "NS", "NS", "NS", "NS", "NS", "NS", "NS", "ES"]]
 
@@ -17,8 +18,9 @@ function setup() {
 }
 
 function draw() {
-  grid.draw();
+
   mouse.draw();
+  grid.draw();
 }
 
 function found(place, thing) {
@@ -48,7 +50,7 @@ function Grid(walls) {
     forEach2D(this.walls, function(cell, i, j) {
       var d = cellSize, x = j * d, y = i * d;
 
-      fill(bkCol + 10).stroke(bkCol - 10).rect(x, y, d, d);   //outline
+      //fill(bkCol + 10).stroke(bkCol - 10).rect(x, y, d, d);   //outline
       stroke(0).strokeWeight(1);
       if (found(cell, 'N')) line(x, y, x + d, y);
       if (found(cell, 'E')) line(x + d, y, x + d, y + d);
@@ -69,10 +71,10 @@ function Character(row, col) {
 
   this.getNeighbour = function(row, col, dir) {
     switch(dir) {
-      case 'N': try {return this.map[row][col-1]} catch (e) {return null}
-      case 'E': try {return this.map[row+1][col]} catch (e) {return null}
-      case 'S': try {return this.map[row][col+1]} catch (e) {return null}
-      case 'W': try {return this.map[row-1][col]} catch (e) {return null}
+      case 'N': try {return this.map[row-1][col]} catch (e) {return null}
+      case 'E': try {return this.map[row][col+1]} catch (e) {return null}
+      case 'S': try {return this.map[row+1][col]} catch (e) {return null}
+      case 'W': try {return this.map[row][col-1]} catch (e) {return null}
     }
   }
 
@@ -86,7 +88,7 @@ function Character(row, col) {
   this.getAccessibleDirections = function(row, col) {
     var me = this;
     return ['N','E','S','W'].filter(function(dir) {
-      return found(me.map[row][col].walls, dir);
+      return !found(me.map[row][col].walls, dir);
     });
   }
 
@@ -95,17 +97,27 @@ function Character(row, col) {
   }
 
   this.seek = function(row, col) {
-    var dest = this.map[row][col];
-    dest.counted = 1;
-    this.getAccessibleNeighbours(row, col).forEach(function(cell) {
-      cell.counted = 1;
-      cell.distance = dest.distance + 1;
-    })
+    var me = this;
+
+    function recurse(row, col, dist) {
+      var cell = me.map[row][col];
+      if (!cell.counted) {
+        cell.counted = 1;
+        cell.distance = dist + 1;
+        maxDist = (cell.distance > maxDist ? cell.distance : maxDist);
+        //fill(cell.counted*255).strokeWeight(0).textAlign(CENTER,CENTER).text(String(cell.distance), cell.x + offset, cell.y + offset);
+        me.getAccessibleNeighbours(row, col).forEach(function(child) {
+          recurse(child.row, child.col, cell.distance);
+        });
+      }
+    }
+    recurse(row, col, 0);
   }
 
   this.draw = function() {
     fill(255, 204, 0).stroke(0).strokeWeight(1).ellipse(this.x, this.y, this.diameter, this.diameter);
     forEach2D(this.map, function(cell, i, j) {
+      fill(cell.distance / maxDist * 255).stroke(bkCol - 10).rect(cell.x, cell.y, cell.x + cellSize, cell.y + cellSize);
       fill(cell.counted*255).strokeWeight(0).textAlign(CENTER,CENTER).text(String(cell.distance), cell.x + offset, cell.y + offset);
     });
   }
